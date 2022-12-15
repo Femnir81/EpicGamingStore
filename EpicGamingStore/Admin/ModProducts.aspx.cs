b57ae4583d9c1237995106352d5ea13742762c89
dimitrilzk <dimitrilazuka95@gmail.com>
2022-12-15 17:32:48 +0100

    inizio

--- EpicGamingStore/Admin/ModProducts.aspx.cs
+++ EpicGamingStore/Admin/ModProducts.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +13,48 @@ namespace EpicGamingStore.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            int id = Convert.ToInt32(Request.QueryString["idprod"]);
+            if(!IsPostBack)
+            {
+                
+            }
+        }
+
+        public void GetProduct(int id)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConnStringEpicGaming"].ToString();
+            con.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.CommandText= $"select * ProdottiTab where IDProdotto = {id}";
+            command.Connection= con;
+            SqlDataReader reader= command.ExecuteReader();
+            if(reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    Nome.Value = reader["NomeProdotto"].ToString();
+                    Descrizione.Value = reader["DescrizioneProdotto"].ToString();
+                    foreach(ListItem cb in CheckBoxPiattaforma.Items)
+                    {
+                        if(reader["Piattaforma"].ToString().ToUpper().Contains(cb.Text.ToUpper()))
+                        {
+                            cb.Selected= true;
+                        }
+                    }
+                    PrezzoIntero.Value = reader["PrezzoIntero"].ToString();
+                    PrezzoSaldo.Value = reader["PrezzoSaldo"].ToString();
+                    InSaldo.Checked = Convert.ToBoolean(reader["InSaldo"]);
+                    Sviluppatore.Value = reader["Sviluppatore"].ToString();
+                    Publisher.Value = reader["Publisher"].ToString();
+                    DataRilascio.Value = reader["DataRilascio"].ToString();
+                    front.Text = reader["URLImg"].ToString();
+
+
+
+                }
+            }
 
         }
     }
